d6ca05936bb839f78e21789d2ce4bc195d10b09d
realetech <realetech@users.noreply.github.com>
2022-01-25 16:12:10 +0000

    lab14 completed

---
+++ advlist/animal-list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def main():
+
+    animals = ["Fox", "Bat", "Rat", "Cat", "Ant", "Bee", "Cod", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog"]
+    print(animals)
+
+    #print bat
+    print(animals[1])
+
+    #print dog and cat
+    print(animals[7], animals[3])
+    
+main()
