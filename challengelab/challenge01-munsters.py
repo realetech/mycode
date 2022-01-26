899340417d5e2dc376b9dbd11a79dd7b4c00208a
realetech <realetech@users.noreply.github.com>
2022-01-26 17:16:18 +0000

    lab 63 completed

---
+++ challengelab/challenge01-munsters.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Alta3 Research | RZFeeser
+   Review of Lists and Dictionaries"""
+
+# define a short data set (in real world, we want to read this from a file or API)
+munsters = {'endDate': 1966, 'startDate': 1964,\
+        'names':['Lily', 'Herman', 'Grandpa', 'Eddie', 'Marilyn'], 'episodes': 70} #tes dict
+
+# Your solution goes below this line
+# ----------------------------------
+
+print(munsters["names"])
+print(munsters["endDate"])
+print(munsters["startDate"])
+print(munsters["episodes"])
+
+
